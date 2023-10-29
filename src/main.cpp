2405bbb03d62c07a13c9dc462b05c2aa28851f05
Warvan1 <collinsn@clarkson.edu>
2023-10-29 15:06:00 -0400

    added multithreading to jobQueueThread

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <mutex>
 #include <list>
+#include <algorithm>
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
@@ -18,20 +19,50 @@ std::mutex tLock;
 
 // runs in a seperate thread
 // if there is a job in the queue it gets ran and then removed from the front of the queue in a loop
-void jobQueueThread(std::list<std::string> &queue, json &config){
+void jobQueueThread(std::list<std::string> &queue, json &config, std::size_t maxThreads){
     while(true){
         tLock.lock();
         bool queueEmpty = queue.empty();
         tLock.unlock();
 
         if(!queueEmpty){
-            //run the first job in the queue
-            syncProject(queue.front(), config[queue.front()]);
-            //remove first item from queue
+            //create thread pool
+            std::vector<std::thread> syncThreads;
+            //create currientJobs vector to keep track of what jobs we are curriently syncing so that we dont do the same one at the same time.
+            std::vector<std::string> currientJobs;
+            //calculate the currient number of needed threads
             tLock.lock();
-            queue.pop_front();
-            std::cout << queue.size() << std::endl;
+            std::size_t numThreads = std::min(maxThreads, queue.size());
             tLock.unlock();
+
+            //run up to "maxThreads" jobs in the queue
+            for(std::size_t i = 0; i < numThreads; i++){
+                //retrieve and then remove first item from queue
+                tLock.lock();
+                std::string jobName = queue.front();
+                queue.pop_front();
+                std::cout << queue.size() << std::endl;
+                tLock.unlock();
+
+                //check to make sure that jobName is not in currientJobs already
+                bool abortSync = false;
+                for(int i = 0; i < currientJobs.size(); i++){
+                    if(currientJobs[i] == jobName){
+                        abortSync = true;
+                        break;
+                    }
+                }
+
+                if(abortSync == false){
+                    currientJobs.push_back(jobName);
+                    syncThreads.push_back(std::thread(syncProject, jobName, std::ref(config[jobName])));
+                }
+            }
+
+            //join all our threads before we continue
+            for(std::size_t i = 0; i < syncThreads.size(); i++){
+                syncThreads[i].join();
+            }
         }
 
         //sleep for 5 seconds so that we arnt running constantly and to prevent constant locking
@@ -55,7 +86,7 @@ int main(){
     std::list<std::string> queue;
 
     //jobqueueThread runs all the jobs that get entered into the queue
-    std::thread jt(jobQueueThread, std::ref(queue), std::ref(config));
+    std::thread jt(jobQueueThread, std::ref(queue), std::ref(config), 4);
 
     std::vector<std::string> *name;
     int seconds_to_sleep;

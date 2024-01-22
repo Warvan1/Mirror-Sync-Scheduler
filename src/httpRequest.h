f134da87536681ae5c95a0caa97da7813f263017
warvan <collinsn@clarkson.edu>
2024-01-22 11:30:59 -0500

    revert changes related to using mirrorapi for mirrors.json

--- src/httpRequest.h
+++
@@ -1,8 +0,0 @@
-// this library was adapted from this stackoverflow page
-// https://stackoverflow.com/questions/70550983/http-request-using-sockets-on-c
-
-#pragma once
-
-std::string http_request(std::string host, int port, std::string resource, std::string query);
-
-std::string remove_header(std::string response);
